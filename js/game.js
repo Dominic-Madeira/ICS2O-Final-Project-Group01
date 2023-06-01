79e4e974d5537af7fb5e16ea4b922db2052fe2df
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-06-01 16:50:16 +0000

    Second edit

---
+++ js/game.js
@@ -0,0 +1,7 @@
+/* global Phaser */
+
+//* Copyright (c) 2023 Dominic M. Mohamad T. All rights reserved
+//
+// Created by: Dominic M. Mohamad T.
+// Created on: June 2023
+// This is the Phaser3 game configuration file
