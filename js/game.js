68ee9c0002be2c728b89a6597540baff3649ede5
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-06-01 17:31:31 +0000

    Added JS

--- js/game.js
+++ js/game.js
@@ -5,3 +5,40 @@
 // Created by: Dominic M. Mohamad T.
 // Created on: June 2023
 // This is the Phaser3 game configuration file
+
+// scene import statements
+import SplashScene from './scenes/splashScene.js'
+import TitleScene from './scenes/titleScene.js'
+
+// create the scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+
+//* Game scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+
+// load scenes
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+
+// start title
+game.scene.start('splashScene')
---
+++ js/titleScene.js
@@ -0,0 +1,53 @@
+// /* global Phaser */
+
+// // Copyright (c) 2023 Dominic M. Mohamad T. All rights reserved
+// //
+// // Created by: Dominic M. Mohamad T.
+// // Created on: June 2023
+// // This is the Title Scene
+
+// class TitleScene extends Phaser.Scene {
+//     constructor() {
+//       super({ key: "titleScene" });
+  
+//       this.titleSceneBackgroundImage = null;
+//     }
+  
+//     /**
+//      * Can be defined on your own Scenes.
+//      * This method is called by the Scene Manager when the scene starts,
+//      *   before preload() and create().
+//      *@param {object} data - Data passed via ScenePlugin.add() or ScenePlugin.start().
+//      */
+//     init(data) {
+//       this.cameras.main.setBackgroundColor("#ffffff");
+//     }
+  
+//     /**
+//      * Can be defined on your own Scenes.
+//      * Use it to load assets.
+//      */
+//     preload() {
+//       console.log("Title Scene");
+//     }
+  
+//     /**
+//      * Can be defined on your own Scenes.
+//      * Use it to create your game objects.
+//      * @param {object} data - Data passed via ScenePlugin.add() or ScenePlugin.start().
+//      */
+//     create(data) {
+//     }
+  
+//     /**
+//      * Should be overridden by your own Scenes.
+//      * This method is called once per game step while the scene is running.
+//      * @param {number} time - The current time.
+//      * @param {number} delta - The delta time in ms since the last frame.
+//      */
+//     update(time, delta) {
+//     }
+//   }
+  
+//   export default TitleScene;
+  
